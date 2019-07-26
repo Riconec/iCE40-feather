b148b5febb59b6973be43d2a36291b0a379b5876
Josh Johnson <josh@joshajohnson.com>
2019-07-26 12:43:38 +1000

    generating BOM and costing things

---
+++ gateware/testScript/clockDividerHertz.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module clockDividerHz #(
+	parameter integer FREQUENCY = 1
+	)(
+	input clk,
+	input rst,
+	input enable,
+	output reg dividedClk = 0,
+	output reg dividedPulse = 0
+);	
+
+	localparam CLK_FREQ = 32'd12_000_000;
+	localparam THRESHOLD = CLK_FREQ / FREQUENCY;
+	
+	reg [31:0] counter;
+
+	always @(posedge clk) begin
+		if (rst | counter >= THRESHOLD - 1) begin
+			counter = 0;
+		end
+		else if (enable) begin
+			counter = counter + 1;
+		end
+	end
+
+	always @(posedge clk) begin
+		if (rst) begin
+			dividedClk = 0;
+			dividedPulse = 0;
+		end
+		else if (counter >= THRESHOLD - 1) begin
+			dividedClk = ~dividedClk;
+			dividedPulse = 1;
+		end
+	end
+endmodule
+
