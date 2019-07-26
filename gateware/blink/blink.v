1ad84b8d3b6f000c24e516ddd39587b07842a725
Josh Johnson <josh@joshajohnson.com>
2019-07-26 21:17:51 +1000

    keep uart leds off while not in use

--- gateware/blink/blink.v
+++ gateware/blink/blink.v
@@ -3,7 +3,9 @@
 
 module top(
     input clk,
-    output nLED_RED
+    output nLED_RED,
+    output UART_RX,
+    output UART_TX
 );
 
 	wire dividedClk;
@@ -19,6 +21,10 @@ module top(
 		);
 
 	assign nLED_RED = dividedClk;
+
+	// Keep UART LEDs off
+	assign UART_RX = 0;
+	assign UART_TX = 0;
 
 endmodule
 
